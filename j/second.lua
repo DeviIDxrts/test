9c76d51d905898b7e41bd186ae7490a27da3a14a
DeviIDxrts <115700883+DeviIDxrts@users.noreply.github.com>
2025-05-15 17:09:23 +0200

    Create second.lua

---
+++ j/second.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DeviIDxrts/test/refs/heads/main/test.lua"))()
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DeviIDxrts/test/refs/heads/main/j/main.lua"))()
